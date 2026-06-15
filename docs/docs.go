0fc38f53fb7b5296475eb8391ac0322bee03d408
涵曦 <im.hanxi@gmail.com>
2026-06-15 08:53:50 +0800

    chore: release version 2.8.3

--- docs/docs.go
+++ docs/docs.go
@@ -7216,7 +7216,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "2.8.2",
+	Version:          "2.8.3",
 	Host:             "localhost:58091",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
